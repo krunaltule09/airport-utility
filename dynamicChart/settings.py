f7fb450437df6321c0c988a10a62f4434b6b6502
krunal <krunaltule53@gmail.com>
2019-12-04 16:42:16 +0530

    initial commit

--- dynamicChart/settings.py
+++ dynamicChart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'hw1_iy9t+(^8&#7f(fgo=u843jylq0uv*sym4cim7j!5vspp%6'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['https://pollutionmonitoring.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
